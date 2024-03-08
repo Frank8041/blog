ff2d70bfe0d9cf00a13458e45504f3fcfcf5afcb
nzemia <mualukofrank@gmail.com>
2024-03-08 23:23:48 +0300

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -25,5 +26,38 @@ export const signup = async (req, res, next) => {
         } catch (error) {
             next(error);            
         }
+};
 
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if( !email || !password ||
+        email === "" || password === ""){
+            next(errorHandler(400, "All fields are required!"));
+        }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(400, "User not found!"));            
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid password!"));
+        }
+
+        // if details are correct, ...
+        const token = jwt.sign(
+            { id: validUser._id }, process.env.JWT_SECRET
+        );
+        // we dont want to send back the password
+        const { password: pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie("access_token", token, {
+            httpOnly: true}).json(rest);
+        
+    } catch (error) {
+        next(error);
+        
+    }
 }
