f196007f988942b7f6ce4f3efb66b8e09e82f8f9
nzemia <mualukofrank@gmail.com>
2024-03-14 01:21:45 +0300

    update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create, deletepost, getposts } from "../controllers/post.controllers.js";
+import { create, deletepost, getposts, updatepost } from "../controllers/post.controllers.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 //here, we dont want to verify the user, because everyone without authentication can search the posts
 router.get("/getposts", getposts);
+
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router;
