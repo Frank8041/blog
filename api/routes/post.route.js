a6b0bb1b59cd0971995b8d22358fc43fde801d3a
nzemia <mualukofrank@gmail.com>
2024-03-13 10:12:54 +0300

    getposts api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controllers.js";
+import { create, getposts } from "../controllers/post.controllers.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+//here, we dont want to verify the user, because everyone without authentication can search the posts
+router.get("/getposts", getposts);
 
 export default router;
