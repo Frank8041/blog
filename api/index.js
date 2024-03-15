079066a29440aacd60b93a2486eb17187278d27f
nzemia <mualukofrank@gmail.com>
2024-03-16 00:16:53 +0300

    complete comment section

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import commentRoutes from "./routes/comment.route.js";
 import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
@@ -23,6 +24,8 @@ app.use(express.json()); // allows you to send json to b
 
 app.use(cookieParser());
 
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 });
@@ -32,6 +35,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use("/api/post", postRoutes);
+
+app.use('/api/comment', commentRoutes);
 
 //middleware
 app.use((err, req, res, next) => {
