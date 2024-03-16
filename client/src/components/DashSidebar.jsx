ce1977927431f066f40c86645fc26a2c5998460a
nzemia <mualukofrank@gmail.com>
2024-03-16 10:50:51 +0300

    add dashboard overview to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -38,8 +38,21 @@ export default function DashSidebar() {
     }
     return (
         <Sidebar className="w-full md:w-56">
-            <Sidebar.Items>
+            <Sidebar.Items>                
                 <Sidebar.ItemGroup className="flex flex-col gap-2">
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to="/dashboard?tab=dash">
+                                <Sidebar.Item
+                                    active={tab === "dash" || !tab}
+                                    icon={HiChartPie}                                    
+                                    as="div"
+                                >
+                                    Dashboard
+                                </Sidebar.Item>
+                            </Link> 
+                        )
+                    }
                     <Link to="/dashboard?tab=profile">
                         <Sidebar.Item 
                             active={tab === "profile"} 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComponent from "../components/DashboardComp";
 
 
 export default function Dashboard() {
@@ -37,6 +38,9 @@ export default function Dashboard() {
 
             {/* comments */}
             {tab === 'comments' && <DashComments />}
+
+            {/* dashboard component */}
+            {tab === 'dash' && <DashboardComponent />}
 
         </div>
     )
